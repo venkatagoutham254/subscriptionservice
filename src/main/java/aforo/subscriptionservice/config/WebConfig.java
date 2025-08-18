0f922f5aea70c63f31d0696e287fe7d384d6ccfc
gowtham@aforo.ai <gowtham@aforo.ai>
2025-08-18 17:47:18 +0530

    added cors (#3)

---
+++ src/main/java/aforo/subscriptionservice/config/WebConfig.java
@@ -0,0 +1,33 @@
+package aforo.subscriptionservice.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://productscreens.s3-website-ap-northeast-1.amazonaws.com",
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "http://localhost:3002",
+                        "http://localhost:3003",
+                        "http://localhost:3004",
+                        "http://localhost:3005",
+                        "http://13.115.248.133",
+                        "http://54.238.204.246",
+                        "http://18.182.19.181"
+
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
